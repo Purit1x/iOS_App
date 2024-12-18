6cc2988aea86e9313c7f79361b8075a265f906be
Purit1x <xuy_eason@foxmail.com>
2024-12-18 14:45:48 +0800

    Initial commit.

--- CheckIn/CheckInApp.swift
+++ CheckIn/CheckInApp.swift
@@ -16,35 +16,5 @@ struct CheckInApp: App {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
-        .windowStyle(.automatic)
-        .commands {
-            SidebarCommands()
-            CommandGroup(after: .newItem) {
-                Button("新建签到窗口") {
-                    openNewWindow()
-                }
-                .keyboardShortcut("n", modifiers: [.command, .shift])
-            }
-        }
-        
-        WindowGroup("签到详情") {
-            CheckInDetailWindow()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
-        }
-        .windowStyle(.automatic)
-    }
-    
-    private func openNewWindow() {
-        let url = URL(string: "checkin://new")!
-        UIApplication.shared.open(url, options: [:], completionHandler: nil)
-    }
-}
-
-struct CheckInDetailWindow: View {
-    var body: some View {
-        NavigationView {
-            Text("请从主窗口选择签到记录")
-                .navigationTitle("签到详情")
-        }
     }
 }
